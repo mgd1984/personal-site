cc491ec0256ba88f4f55cb38df410744821878d5
ajm <alecjohnmiller@gmail.com>
2022-03-21 19:54:16 -0400

    updated resume

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -17,6 +17,9 @@ const positions = [
     link: 'https://www.rogers.com',
     daterange: '2019 - 2020',
     points: [
+      'Developed and delivered data-driven marketing campaigns to Small Business Wireless customers, maximizing subscriber revenue and customer retention through an analytical approach to consumer marketing.',
+      'Managed the execution of ongoing subscriber revenue initiatives including base pricing and device financing policy changes to drive key insights and strategic outcomes.',
+      'Worked with internal stakeholders and external marketing agencies to identify and engage customers using a variety of propensity models and lenses such as contract status, value tiers, geography, and device.',
     ],
   },
   {
@@ -25,6 +28,9 @@ const positions = [
     link: 'https://www.rogers.com',
     daterange: '2019',
     points: [
+      'Led the analysis of numerous business cases related to acquisition pricing, promotions, and competitive dynamics to provide telecom services to small business wireless customers in a profitable manner.',
+      'Contributed to dozens of cross-functional projects involving legal, product, marketing, data, finance, and operations, including the launch of Rogers Infinite – the first unlimited voice & data plans in Canada.',
+      'Developed automated data pipelines using SAS/SQL, Microsoft Excel, PowerQuery, and PowerBI to ensure timely, accurate, and reproducible data flows for analysis and insight.',
     ],
   },
   {
@@ -33,6 +39,10 @@ const positions = [
     link: 'https://www.rogers.com',
     daterange: '2018-2019',
     points: [
+      'Provided analytical support into wireless pricing and product mix for a small business wireless base consisting of hundreds of thousands of subscribers.',
+      'Gathered historical customer data and built Excel models of customer behaviour to inform business cases around pricing, promotional, and competitive activities.',
+      'Used a variety of advanced analytical techniques such as EDA, sampling, dimensionality reduction, and data models to distill key insights from massive amounts of data.',
+      'Conducted post-implementation reviews for wireless acquisition and base management campaigns to measure impact and performance relative to original estimates.',
     ],
   },
   {
@@ -41,6 +51,9 @@ const positions = [
     link: 'https://marsdd.com',
     daterange: '2013-2015',
     points: [
+      'Managed a multi-party data-sharing partnership between Regional Innovation Centres in Ontario to focus and enhance the startup investment decision-making process using data and analytics.',
+      'Collaborated with a team of data scientists, application developers and product managers to collect, combine and visualize new venture data into analytical products for key partners at Regional Innovation Centres (part of the Ontario Network of Entrepreneurs).',
+      'Contributed to data partnership governance including strategy development, program reporting, budgeting, resource planning, contract review, intellectual property assessment and product development.',
     ],
   },
   {
@@ -49,6 +62,9 @@ const positions = [
     link: 'https://techalliance.ca',
     daterange: '2012-2013',
     points: [
+      'Created and implemented a client management system using salesforce.com to assess, monitor and prioritize client company progress which improved analysis and decision-making on the Advisory Services team.',
+      'Developed tools, programs and dashboards to connect startups with market-readiness resources (market intelligence, funding opportunities, service providers, education, workshops, etc.) which enabled the Advisory Services team to hire sector-specific analysts leading to increased client capacity and service offerings.',
+      'Blogger, conference speaker, and guest lecturer at the Richard Ivey School of Business on various innovation and entrepreneurial topics such as crowdfunding, 3D printing and entrepreneurial success factors.',
     ],
   },
   {
@@ -57,6 +73,9 @@ const positions = [
     link: 'https://techalliance.ca',
     daterange: '2008-2012',
     points: [
+      'Worked with Advisory Services team and Southwestern Ontario business community to deliver market readiness programs to over 125 entrepreneurs and technology start-ups through the Business Acceleration Program - an economic development initiative of the Ontario Ministry of Research and Innovation targeted at technology companies in the ICT, cleantech, life science and advanced manufacturing industries.',
+      'Assisted companies in raising private and public capital totaling in excess of $2.5 million.',
+      'Acted as liaison for entrepreneurs to access start-up resources, reviewed business plans and consulted with early-stage technology companies.',
     ],
   },
 ];
