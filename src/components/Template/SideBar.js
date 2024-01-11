34a12d303ed412f89f829c6baf02519e19f6a350
mgd1984 <alecjohnmiller@gmail.com>
2024-01-11 13:25:51 -0500

    Refactor SideBar.js to improve readability and organization

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,18 +19,20 @@ const SideBar = () => (
     <section className="blurb">
       <h2>About</h2>
       <p>
-        Hi, I&apos;m Alec. I like to make stuff &amp; including businesses, software, and music, and generative art.
-        I&apos;m an <a href="https://ivey.uwo.ca/">Ivey School of Business</a> and <a href="https://learn.utoronto.ca/programs-courses/certificates/data-science">University of Toronto SCS </a> graduate
-        with specializations in Entrepreneurship &amp; Data Science.
-        I&apos;m the owner/operator of <a href="https://millhaus.ca">Millhaus Technology Services</a>, prior to which I worked in computer sales, data partnerships,
-        telecom pricing analytics, and startup incubation including roles @ <a href="https://rogers.com">Rogers</a>,
-        <a href="https://marsdd.com">MaRS</a>,
-        <a href="https://techalliance.ca">TechAlliance</a>,
-        and <a href="https://lenovo.com">Lenovo</a>.
-      </p>
-      <p>
-        &copy; Alec Miller <Link to="/">mgd1984.github.io/personal-site</Link>.
-      </p>
+        <p>
+          Hi, I&apos;m Alec. I like to make stuff &amp; including businesses, software, and music, and generative art.
+        </p>
+        <p>
+          I&apos;m an <a href="https://ivey.uwo.ca/">Ivey School of Business</a> and <a href="https://learn.utoronto.ca/programs-courses/certificates/data-science">University of Toronto SCS </a> graduate
+          with specializations in Entrepreneurship &amp; Data Science.
+        </p>
+        <p>
+          I&apos;m the owner/operator of <a href="https://millhaus.ca">Millhaus Technology Services</a>, prior to which I worked in computer sales, data partnerships,
+          telecom pricing analytics, and startup incubation including roles @ <a href="https://rogers.com">Rogers</a>,
+          <a href="https://marsdd.com">MaRS</a>,
+          <a href="https://techalliance.ca">TechAlliance</a>,
+          and <a href="https://lenovo.com">Lenovo</a>.
+        </p>
       <ul className="actions">
         <li>
           {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
