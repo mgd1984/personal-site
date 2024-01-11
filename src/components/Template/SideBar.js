8c4410321aab77390ba1152a21a96fdcbc3d5aa1
mgd1984 <alecjohnmiller@gmail.com>
2024-01-11 13:22:26 -0500

    Update SideBar.js with additional information

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -18,14 +18,15 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Alec. I like to make stuff &amp; including businesses, software, and music, and generative art.
+      <p>
+        Hi, I&apos;m Alec. I like to make stuff &amp; including businesses, software, and music, and generative art.
         I&apos;m an <a href="https://ivey.uwo.ca/">Ivey School of Business</a> and <a href="https://learn.utoronto.ca/programs-courses/certificates/data-science">University of Toronto SCS </a> graduate
         with specializations in Entrepreneurship &amp; Data Science.
         I&apos;m the owner/operator of <a href="https://millhaus.ca">Millhaus Technology Services</a>, prior to which I worked in computer sales, data partnerships,
-        telecom pricing analytics, and startup incubation including roles @ <a href="https://rogers.com">Rogers</a>
-        , <a href="https://marsdd.com">MaRS</a>
-        , <a href="https://techalliance.ca">TechAlliance</a>
-        , and <a href="https://lenovo.com">Lenovo</a>.
+        telecom pricing analytics, and startup incubation including roles @ <a href="https://rogers.com">Rogers</a>,
+        <a href="https://marsdd.com">MaRS</a>,
+        <a href="https://techalliance.ca">TechAlliance</a>,
+        and <a href="https://lenovo.com">Lenovo</a>.
       </p>
       <ul className="actions">
         <li>
@@ -36,7 +37,9 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Alec Miller <Link to="/">mgd1984.github.io/personal-site</Link>.</p>
+      <p>
+        &copy; Alec Miller <Link to="/">mgd1984.github.io/personal-site</Link>.
+      </p>
     </section>
   </section>
 );
